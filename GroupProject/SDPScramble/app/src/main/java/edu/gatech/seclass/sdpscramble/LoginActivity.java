5478ff7dade5a83e054635ef6280cf1f1f98ea47
Youjung Kim <ykim691@gatech.edu>
2017-10-09 19:38:34 -0400

    2nd Commit after changing EWS directory 2nd Commit after changing EWS directory 2nd Commit after changing EWS directory

--- GroupProject/SDPScramble/app/src/main/java/edu/gatech/seclass/sdpscramble/LoginActivity.java
+++ GroupProject/SDPScramble/app/src/main/java/edu/gatech/seclass/sdpscramble/LoginActivity.java
@@ -1,7 +1,7 @@
 package edu.gatech.seclass.sdpscramble;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +9,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //ExternalWebService getInstance()
     }
 }
