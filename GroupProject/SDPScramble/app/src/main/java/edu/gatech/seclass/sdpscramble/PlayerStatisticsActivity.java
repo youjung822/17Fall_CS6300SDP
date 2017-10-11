5828314b00718ac87488120a680977c648add895
John Youngblood <jyoungblood8@gatech.edu>
2017-10-10 22:45:08 -0400

    Added skeleton Class files

---
+++ GroupProject/SDPScramble/app/src/main/java/edu/gatech/seclass/sdpscramble/PlayerStatisticsActivity.java
@@ -0,0 +1,16 @@
+package edu.gatech.seclass.sdpscramble;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by John Youngblood on 10/10/17.
+ */
+
+public class PlayerStatisticsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
