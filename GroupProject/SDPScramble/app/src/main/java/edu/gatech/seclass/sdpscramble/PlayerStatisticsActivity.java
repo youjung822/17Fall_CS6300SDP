c7f40fb4f705dc6485d9b584287e718747678edd
John Youngblood <jyoungblood8@gatech.edu>
2017-10-13 14:25:17 -0400

    Added Logo to Login Screen

--- GroupProject/SDPScramble/app/src/main/java/edu/gatech/seclass/sdpscramble/PlayerStatisticsActivity.java
+++ GroupProject/SDPScramble/app/src/main/java/edu/gatech/seclass/sdpscramble/PlayerStatisticsActivity.java
@@ -12,6 +12,6 @@ public class PlayerStatisticsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.aaa);
+        setContentView(R.layout.player_statistics);
     }
 }
--- GroupProject/SDPScramble/app/src/main/java/edu/gatech/seclass/sdpscramble/WordScrambleStatisticsActivity.java
+++ GroupProject/SDPScramble/app/src/main/java/edu/gatech/seclass/sdpscramble/WordScrambleStatisticsActivity.java
@@ -5,12 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by John Youngblood on 10/10/17.
+ * Created by Youngblood on 10/13/2017.
  */
 
-public class WordScrambeStatisticsActivity extends AppCompatActivity {
+public class WordScrambleStatisticsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_scramble_statistics);
     }
 }
