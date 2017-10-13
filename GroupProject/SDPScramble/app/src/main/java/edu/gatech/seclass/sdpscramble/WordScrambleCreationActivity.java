09282d9d59514833d4a2e14da1cc04bd9eaa33bb
Tamur Khan <tkhan30@gatech.edu>
2017-10-12 17:04:55 -0700

    adding code to kill keyboard on scramblecreation screen

--- GroupProject/SDPScramble/app/src/main/java/edu/gatech/seclass/sdpscramble/WordScrambleCreationActivity.java
+++ GroupProject/SDPScramble/app/src/main/java/edu/gatech/seclass/sdpscramble/WordScrambleCreationActivity.java
@@ -1,9 +1,12 @@
 package edu.gatech.seclass.sdpscramble;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import java.net.SocketTimeoutException;
@@ -89,5 +92,12 @@ public class WordScrambleCreationActivity extends AppCompatActivity {
     }
 
     private WordScramble currentWordScramble;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        InputMethodManager inmm = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inmm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
+    }
 }
 
